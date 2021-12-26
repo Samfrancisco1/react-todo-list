1b5deba72ba4d56f6e0e2ad333e8494e76b42115
Yai Francis <yabi.francis001@gmail.com>
2021-12-27 03:58:32 +0800

    Added a footer component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import TodoForm from "./components/TodoForm/TodoForm";
 import TodoList from "./components/TodoList/TodoList";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
-  
+
+  useEffect(() => {
+    saveToLocalStorage();
+  }, [todos])
+
+  //Saving todos in localStorage
+  const saveToLocalStorage = () => {
+    if(localStorage.getItem('todos') === null){
+      localStorage.setItem('todos', JSON.stringify([]))
+    }else{
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }
+  };
+
   return (
     <div className="main">
       <div className="glass">
         <h2>Todo List</h2>
-        <TodoForm setInput={setInput} input={input} todos={todos} setTodos={setTodos} />
+        <TodoForm
+          setInput={setInput}
+          input={input}
+          todos={todos}
+          setTodos={setTodos}
+        />
         <TodoList setTodos={setTodos} todos={todos} />
       </div>
       <div className="circle1"></div>
       <div className="circle2"></div>
+
+      <Footer />
     </div>
   );
 }
